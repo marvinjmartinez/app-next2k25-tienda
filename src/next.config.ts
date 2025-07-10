db902e8caed2d232ed2228a1c0a1be42f1b0ffff
MARVIN MARTINEZ <marvinjmartinez@gmail.com>
2025-07-10 21:12:51 +0000

    nada

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,5 +1,4 @@
 import type {NextConfig} from 'next';
-require('dotenv').config({ path: require('path').resolve(process.cwd(), '.env') });
 
 const nextConfig: NextConfig = {
   /* config options here */
