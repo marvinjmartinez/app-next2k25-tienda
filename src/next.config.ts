db6e1db929696152fcc606dde2b0724ac7602cdc
MARVIN MARTINEZ <marvinjmartinez@gmail.com>
2025-07-10 21:09:57 +0000

    persiste el error

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,4 +1,5 @@
 import type {NextConfig} from 'next';
+require('dotenv').config({ path: require('path').resolve(process.cwd(), '.env') });
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -17,6 +18,9 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  env: {
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
   },
 };
 
