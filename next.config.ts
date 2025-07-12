6570d5c41feab9084184643050b0da5e30beaecf
MARVIN MARTINEZ <marvinjmartinez@gmail.com>
2025-07-12 03:51:18 +0000

    mira

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
