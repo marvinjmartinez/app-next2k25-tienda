43b2006b2176c2a854e7791d822d6bf412e05a68
MARVIN MARTINEZ <marvinjmartinez@gmail.com>
2025-07-12 05:17:59 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: ["*"],
+  },
   env: {
     GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
     FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
